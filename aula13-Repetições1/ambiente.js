03fc3d5f30a04e8b6bcf7c97ede6e75f0504e98e
Tiago Gomes do Nascimento <133234836+DevTiagokings@users.noreply.github.com>
2024-01-05 08:11:30 -0300

    Aula 13- Repetição 1 e Aula 14- Repetições 2

---
+++ aula13-Repetições1/ambiente.js
@@ -0,0 +1,26 @@
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+/*var c = 1
+while (c <= 6){
+    console.log(`passo ${c}`)
+    c++
+    Código de repetição de 1 até 6
+}*/
+
+
+
+/*var c = 1
+while (c <= 3){
+    console.log('passo 1')
+    c++
+    código de repetição simples
+}*/
+
+
+
+/*console.log('Tudo bem?')*/
